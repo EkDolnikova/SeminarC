da867f3b5fa366048cd3c2e54ca0ffdeeb406923
Dolnikova <ek.dolnikowa@yandex.ru>
2022-11-30 23:47:43 +0300

    задача квадрат числа

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго.
+
+//Вводим строковые значения, считываем данные с консоли
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+if (num1Line != null && num2Line != null);  
+
+{
+// Парсим введенное число
+   int num1 = int.Parse(num1Line);
+   int num2 = int.Parse(num2Line);
+
+   if (num2 * num2 == num1)
+   {
+    Console.WriteLine("Да.первое число квадрат второго");
+   }
+   else
+   {
+    Console.WriteLine("Нет,первое число не квадрат второго");
+   }
+}
