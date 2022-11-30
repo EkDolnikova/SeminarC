e59b9ddb3856a3346e44198c2c90f90d97d008b7
Dolnikova <ek.dolnikowa@yandex.ru>
2022-11-30 23:21:51 +0300

    add task about square of the number

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? inputLine = Console.ReadLine(); //Считываем данные с консоли
+
+if(inputLine!=null)  // Проверяем, чтобы данные были не пустыми
+
+{
+   // Парсим введенное число
+   int inputNumber = int.Parse(inputLine);
+
+   // Находим квадрат числа
+   int outNumber = inputNumber*inputNumber;
+
+   //Выводим данные на консоль
+   Console.WriteLine(outNumber);
+}
