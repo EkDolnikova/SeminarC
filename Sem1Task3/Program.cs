dd5cfedde2bfddc4345d8e73108094556ec1fadd
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-01 00:18:50 +0300

    Задача о днях недели

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Необходимо по введенному числу выдать день  недели
+
+string? inputLine = Console.ReadLine(); //считываем данные с консоли. Вопрос значит, что мы проверим на null, но ниже.
+
+if(inputLine!=null) //создаем массив
+{
+string[] dayWeek = new string[7];
+dayWeek[0] = "Понедельник";
+dayWeek[1] = "Вторник";
+dayWeek[2] = "Среда";
+dayWeek[3] = "Четверг";
+dayWeek[4] = "Пятница";
+dayWeek[5] = "Суббота";
+dayWeek[6] = "Воскресенье";
+
+int inputNumber = int.Parse(inputLine); //получаем действительное значение числа
+
+string outDayOfWeek = dayWeek[inputNumber-1]; //находим название дня недели по введенному номеру
+
+Console.WriteLine(outDayOfWeek); //Выводим данные в консоль
+
+}
+
