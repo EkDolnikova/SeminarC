4aad6ef9f7d54f303312b5e3bcd4af004f32b04e
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-03 19:51:36 +0300

    add task 8 DZ

---
+++ Sem1Tesk8DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+string? inputLine = Console.ReadLine(); //считываем дынные с консоли
+if(inputLine!= null);  // проверяем, что данные не пустые
+int inputNumber = int.Parse(inputLine); //делаем число вещественным
+
+int even = 2; // потому, что нужны четные числа
+
+string res = string.Empty; // т.е пустая строка
+
+while (even <= inputNumber)
+{
+    res = res + even + ",";
+    even = even + 2;
+}
+
+Console.WriteLine(res);
+
