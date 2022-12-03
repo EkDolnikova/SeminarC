210e94491e7be717fafb27e2a8f64c260cd91e1b
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-03 17:09:39 +0300

    add task2 DZ

---
+++ Sem1Task2DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+string? num1Line = Console.ReadLine(); // считываем данные с консоли
+string? num2Line = Console.ReadLine();
+
+if (num1Line != null && num2Line != null);  // проверяем, что не нулевые
+
+int num1 = int.Parse(num1Line); // получаем действительные значения числа
+int num2 = int.Parse(num2Line);
+
+if(num1 > num2)
+{
+    Console.WriteLine("Да.первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Нет.Второе число больше первого");
+}
