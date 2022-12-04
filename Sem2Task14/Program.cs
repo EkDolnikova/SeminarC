65847729d972229de6da7a8f9cd44aedb570c20c
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-04 12:48:01 +0300

    add task 14

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+int num = int.Parse(Console.ReadLine()??"0");  // ??"0" - проверка на 0
+
+int res1 = num%7; 
+int res2 = num%23; 
+
+if((res1==0)&&(res2==0)) //&& - и, // - или
+{
+Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
