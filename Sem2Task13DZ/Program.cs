11f8b6c912670a627d9a5e10a402f10f48d641ca
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-04 22:20:09 +0300

    add task 13

---
+++ Sem2Task13DZ/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,что третьей цифры нет.
+
+int num = int.Parse(Console.ReadLine()??"0"); // считываем с консоли и парсим число
+if(num!=null);  //проверяем, чтобы данные были не пустые
+if(num>99)
+{
+    char[] digits = num.ToString().ToCharArray(); // превращаем число в массив
+    Console.WriteLine(digits[2]);
+}
+else Console.WriteLine("Третьего числа нет");
