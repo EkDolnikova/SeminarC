bd2c35fb7434667e63a82ee1ab138843156fa321
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-07 23:54:16 +0300

    add task 19DZ

---
+++ Sem3Task19DZ/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = ReadData("Введите пятизначный палиндром: ");
+
+bool result = PolinTest(num);
+
+Console.WriteLine(result);
+//PrintData(result);
+
+// Метод читает данные от пользователя:
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+// Метод выводит данные пользователю
+//void PrintData(string msg, string result)
+//{
+//    Console.WriteLine(msg + result);
+//}
+
+bool PolinTest(int num)
+{
+    bool result = false;
+    result = (((num/10000== num%10))&&((num/1000)%10 == (num/10) %10));
+    return result;
+}
+
