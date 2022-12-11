0f1e041ae21b13c9965ab7b5bf7cc5d2c4eef59d
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-11 23:34:34 +0300

    add task 26

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// Метод читает данные от пользователя:
+int ReadData(string line)
+{
+    Console.WriteLine(line); // выводит сообщение
+   int numberP = int.Parse(Console.ReadLine()??"0"); //считываем число
+   return numberP; //возвращаем значение
+}
+
+// Метод выводит результат
+void PrinResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Простой метод
+int VariantSimple(int num)
+{
+    int res = 0;
+    while(num >0)
+    {
+        res++;
+        num = num / 10;
+    }
+    return res;
+}
+
+//Метод с логарифмом
+int VariantLog(int num)
+{
+    int count = (int)Math.Log10(num)+1;
+    return count;
+}
+
+//Метод со строкой
+int VarianString(int num)
+{
+    int count = num.ToString().Length;
+    return count;
+}
+
+int number = ReadData("Введите число А:");
+int length = VariantLog(number);
+int length2 = VariantSimple(number);
+int length3 = VarianString(number);
+
+PrinResult("Количество цифр в числе равно(метод с логарифмом): " + length);
+PrinResult("Количество цифр в числе равно(метод простой): " + length2);
+PrinResult("Количество цифр в числе равно(метод со строкой): " + length3);
