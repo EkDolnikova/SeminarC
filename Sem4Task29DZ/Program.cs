164b678282513c3db8947f6144eb883af597d3f8
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-12 22:05:06 +0300

    add task 29 DZ

---
+++ Sem4Task29DZ/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// * Ввести с клавиатуры длину массива и диапазон значений элементов 
+
+// Метод читает данные от пользователя:
+int ReadData(string line)
+{
+    Console.WriteLine(line); // выводит сообщение
+   int numberP = int.Parse(Console.ReadLine()??"0"); //считываем число
+   return numberP; //возвращаем значение
+}
+
+// Метод вывода массива
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i=0;i<arr.Length-1; i++) //Length-1 т,к последняя запятая не нужна
+    {
+    Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length-1] + "]");
+}
+
+int[] GenArr(int lenthArr, int numMin, int numMax)
+{
+        int[] randomArr = new int[lenthArr];
+        Random rnd = new Random();
+    for (int i=0;i<lenthArr; i++)
+    {
+        randomArr[i] = rnd.Next(numMin, numMax + 1);
+    }
+    return randomArr;
+}
+
+int lenthArr = ReadData("Введите длину массива: ");
+int numMin = ReadData("Введите min значение: ");
+int numMax = ReadData("Введите max значение: ");
+//int[] arr = GenArr(arrLen);
+PrintArray(GenArr(lenthArr,numMin,numMax));
