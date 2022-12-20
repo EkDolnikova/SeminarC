0a8fbfdaec68bf6cb9f3e99f76bbf4ccbb0ba90a
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-20 23:27:39 +0300

    add task 41 DZ

---
+++ Sem6Task41DZ/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// * Пользователь вводит число нажатий. Программа следит за нажатиями, затем выдает, сколько чисел больше 0 было введено
+
+int M = ReadData("Введите M суммарное количество чисел: ");
+PrintData(CountPosNum(M));
+
+
+// Метод считывает число с консоли
+int ReadData(string line)
+{
+        Console.WriteLine(line);
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        return number;
+}
+
+// Метод выводит результат
+void PrintData(int number)
+{
+    Console.WriteLine("Количество введённых чисел больше 0: " + number);
+}
+
+// Метод подсччета кол-ва чисел введенных больше 0
+int CountPosNum(int M) 
+{
+    int res = 0;
+    for (int i = 0; i < M; i++)
+    {
+        int num = ReadData("Введите число: ");
+        if (num > 0) res++;
+    }
+    return res;
+}
