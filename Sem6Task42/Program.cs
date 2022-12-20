0073291edebc3a14d63bd4c4dab4ce8b38abae7a
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-20 22:58:02 +0300

    add task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая будет преобразовывать десятичные числа в двоичные.
+
+int inputNumber = ReadData("Enter number: ");
+
+PrintData("Result: " + DegToBin(inputNumber));
+PrintData("Result: " + DecToBinNativ(inputNumber));
+PrintData("Result: " + Convert.ToString(inputNumber, 8));  // конвертация в 8-ричную систему
+PrintData("Result: " + Convert.ToString(inputNumber, 16)); // конвертация в 16-ричную систему
+
+// Считывает с пользователя число
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод речатает результат
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод конвертирует число из десятичной системы в двоичную
+string DegToBin(int num)
+{
+    string outBin = String.Empty;
+    while (num > 0)
+    {
+        outBin = (num % 2) + outBin;
+        num = num / 2;
+    }
+    return outBin;
+}
+
+// Встроенный метод перевода числа из десятичной системы в двоичную
+string DecToBinNativ(int num)
+{
+    return Convert.ToString(num, 2);
+}
