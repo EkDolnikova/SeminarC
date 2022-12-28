0ca4d24e8eb8b2ee899c696181cd82b9ace4b6cf
Dolnikova <ek.dolnikowa@yandex.ru>
2022-12-28 22:50:09 +0300

    add task 54DZ

---
+++ Sem8Task54DZ/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Заполняем массив случайными числами
+void Fill2DArray(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+// Печать двумерного массива
+void Print2DArray(string line, int[,] matr)
+{
+    Console.WriteLine();
+    Console.WriteLine(line);
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+        }
+    Console.WriteLine();
+}
+
+// Метод сортировки массива по убыванию;
+int[,] UpdateArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int buf = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = buf;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+int m = ReadData("Введите количество строк M: ");
+int n = ReadData("Введите количество столбцов N: ");
+int[,] matrix = new int[m, n];
+
+Fill2DArray(matrix, 1, 99);
+Print2DArray("Массив случайных чисел: ", matrix);
+Print2DArray("Массив чисел, упорядоченных по убыванию: ", UpdateArray(matrix));
